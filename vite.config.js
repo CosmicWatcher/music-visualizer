fc2942d12519ba27311ba160e5ef9bce11c36c94
Aryan <aryan.kh88@gmail.com>
2023-09-26 19:28:29 -0400

    local and production base path distinction

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import glsl from "vite-plugin-glsl";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import glsl from 'vite-plugin-glsl';
+
+const ASSET_URL = process.env.ASSET_URL || '';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: ASSET_URL,
   plugins: [react(), glsl()],
   server: {
     hmr: true,
